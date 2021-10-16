b501eb8a8542fcfdc75804bf2c7acd1ce79eed0e
romauniat <92386260+romauniat@users.noreply.github.com>
2021-10-16 10:27:33 -0400

    Update A1.java
    Template starter for A1. See instructions on the document

--- A1.java
+++ A1.java
@@ -7,13 +7,13 @@ Description:
 public class A1{
   
   public static void main(String[] args){
-  
-  //Q1
-  
-  
-  //Q2
-
-
+    
+    //Q1
+    
+    
+    //Q2
+    
+    
   }// close main method
 
 
